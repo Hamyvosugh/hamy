b6b704f667d38add326391e98e49bfbd78348df9
hamy vosugh <hamy.vosugh@gmail.com>
2024-08-24 02:26:37 +0200

    init2 + home update

--- wp-content/themes/hamyvosugh/home.php
+++ wp-content/themes/hamyvosugh/home.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Home
+Template Name:  Homepage
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,10 +12,13 @@ if ( ! class_exists( 'Elementor\Plugin' ) ) {
     wp_die( 'Elementor plugin is required for this template to function properly.' );
 }
 
-// Load Elementor header
-\Elementor\Plugin::instance()->frontend->get_builder_content_for_display();
+// Get the current post ID
+$post_id = get_the_ID();
+
+// Load Elementor content for the current post
+echo \Elementor\Plugin::instance()->frontend->get_builder_content_for_display( $post_id );
 
 while ( have_posts() ) : the_post();
-    // The content will be managed by Elementor
+    // The content will be managed by Elementor, but if you want to output regular content as well:
     the_content();
 endwhile;
