e607e1c6fc8b12d767aac35d30e035e3c6cd801a
hamy vosugh <hamy.vosugh@gmail.com>
2024-08-24 21:35:08 +0200

    hello_elementor_child_scripts_styles

--- wp-content/themes/hamyvosugh/functions.php
+++ wp-content/themes/hamyvosugh/functions.php
@@ -60,6 +60,21 @@ function hamyvosugh_register_menus() {
 add_action( 'init', 'hamyvosugh_register_menus' );
 
 
+// regoister child them
+function hello_elementor_child_scripts_styles() {
+
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		HELLO_ELEMENTOR_CHILD_VERSION
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
 // font
 if ( ! function_exists( 'hamyvosugh_enqueue_styles' ) ) {
     function hamyvosugh_enqueue_styles() {
