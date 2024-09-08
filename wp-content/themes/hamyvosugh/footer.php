fcea8bed2360f4e45b2e6d23fefe3579cbc61749
hamy vosugh <hamy.vosugh@gmail.com>
2024-09-08 12:36:09 +0200

    add new child theme

--- wp-content/themes/hamyvosugh/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="site-footer" class="site-footer" role="contentinfo">
-    <div class="footer-inner">
-    </div><!-- .footer-inner -->
-</footer><!-- #site-footer -->
-
-<?php wp_footer(); ?>
-</body>
-</html>
--- wp-content/themes/hamyvosugh/single.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-        the_content(); // This is where Elementor will inject its content
-    endwhile;
-else :
-    echo '<p>No content found</p>';
-endif;
-?>
